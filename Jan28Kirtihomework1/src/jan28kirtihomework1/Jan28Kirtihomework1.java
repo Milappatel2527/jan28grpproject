f279e1ba351ce46b56682dd4a787d1f2a7f732d8
KirtiG57 <kgangwar57@gmail.com>
2021-01-28 22:42:01 -0600

    homework

--- Jan28Kirtihomework1/src/jan28kirtihomework1/Jan28Kirtihomework1.java
+++ Jan28Kirtihomework1/src/jan28kirtihomework1/Jan28Kirtihomework1.java
@@ -16,7 +16,7 @@ public class Jan28Kirtihomework1 {
      */
     public static void main(String[] args) {
           // TODO code application logic here
-          System.out.println("Hello Everyone");
+          System.out.println("Hello Everyone ");
     }
     
 }
